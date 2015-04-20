503f1b1a7472d23464445d442d448cc76e4acb3f
david-zhou <david.zhou.tan@gmail.com>
2015-04-20 16:38:52 -0500

    R laser

---
+++ Assets/LaserRmovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserRmovement : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			//Destroy (gameObject);
+			Explosion();
+		}
+	}
+
+	void Explosion()
+	{
+		Vector2 center = new Vector2(transform.position.x, transform.position.y);
+		float radius = 3;
+		Collider2D [] hitColliders = Physics2D.OverlapCircleAll (center, radius);
+
+		for (int i = 0; i< hitColliders.Length; i++) 
+		{
+			if(hitColliders[i].gameObject.tag == "Crane" || hitColliders[i].gameObject.tag == "Pegasus" || hitColliders[i].gameObject.tag == "Dragon")
+			{
+				hitColliders[i].gameObject.SendMessage("ApplyDamage");
+			//Destroy (hitColliders[i].gameObject);
+			}
+		}
+		Destroy (gameObject);
+	}
+}
