1d0df91a58409cd5ad64c611b7ee85b848080f21
Daniel Arturo Rodriguez Rosas <darr.0725@gmail.com>
2015-04-20 17:05:02 -0600

    :rocket: Dinamic background added, buildings over mountains
    Script handles the background speed

---
+++ Assets/scripts/backgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundScroll : MonoBehaviour {
+	
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+	
+	void Start () {
+		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+	
+	void Update () {
+		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (x, savedOffset.y);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+	
+	void OnDisable () {
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}
+}
