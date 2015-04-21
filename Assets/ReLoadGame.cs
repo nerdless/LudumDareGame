02e229206467ad04251328d1c141175a6aac7a5b
Eduardo Ochoa <eduardo8af1@gmail.com>
2015-04-20 22:02:48 -0500

    reload level

---
+++ Assets/ReLoadGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ReLoadGame : MonoBehaviour {
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey (KeyCode.Space))
+			Application.LoadLevel (1);
+	}
+}
